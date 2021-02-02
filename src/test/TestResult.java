3921c24cc59555520f039d257fd1c45979362528
Hizzze <75045730+Hizzze@users.noreply.github.com>
2021-02-02 20:06:55 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -6,7 +6,7 @@ public class TestResult {
 
     public static void main(String[] args) {
         int[] arr = {128, 1241, 123, 4142};
-        System.out.printf("Друге за величиною число у заданому масив: %d",
+        System.out.printf("The second largest in a given array: %d",
                 Exercise.Calculate(arr));      
     }
 }
